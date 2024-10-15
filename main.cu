be6bd1c53c7e140a0bed6cdae08037555cbdfdf1
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2024-10-15 17:38:00 -0400

    Init commit

---
+++ main.cu
@@ -0,0 +1,334 @@
+#include <iostream>
+
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+
+#include <cooperative_groups/memcpy_async.h>
+#include <cuda/cmath>
+#include <cuda/std/array>
+#include <cuda/std/chrono>
+#include <cutlass/epilogue/thread/activation.h>
+#include <fmt/ranges.h>
+#include <cuda.h>
+#include <nccl.h>
+#include <nvshmemx.h>
+#include <nvshmem.h>
+#include <host/nvshmemx_api.h> // Makes CLion happy
+#include <torch/torch.h>
+#include "processor/gemm.cuh"
+
+#define NANO_TO_MICRO (cuda::std::nano::den / cuda::std::micro::den)
+#if !defined(CHECK_ERROR_EXIT)
+#  define CHECK_ERROR_EXIT(e)                                         \
+do {                                                           \
+    cudaError_t code = (e);                                      \
+    if (code != cudaSuccess) {                                   \
+    fprintf(stderr, "<%s:%d> %s:\n    %s: %s\n",               \
+    __FILE__, __LINE__, #e,                            \
+    cudaGetErrorName(code), cudaGetErrorString(code)); \
+    fflush(stderr);                                            \
+    exit(1);                                                   \
+    }                                                            \
+} while (0)
+#endif
+
+constexpr unsigned int len = 10000U;
+template<unsigned int n>
+__device__ __forceinline__
+unsigned int blockManipulation(const cuda::std::array<bool, n>& isRemote, const unsigned int& idx) {
+    unsigned int numPeers = 0U;
+    cuda::std::array<unsigned int, n> peers{};
+#pragma unroll
+    for(unsigned int i = 0U; i < n; ++i) {
+        const bool b = (idx > 0) * !isRemote[i] + isRemote[i] * (idx == 0);
+        peers[numPeers] = !b * peers[numPeers] + i * b;
+        numPeers += b;
+    }
+    return numPeers;
+}
+
+template<unsigned int n>
+__device__ __forceinline__
+unsigned int blockManipulationBranch(const cuda::std::array<bool, n>& isRemote, const unsigned int& idx) {
+    unsigned int numPeers = 0U;
+    cuda::std::array<unsigned int, n> peers{};
+#pragma unroll
+    for(unsigned int i = 0U; i < n; ++i) {
+        if ((isRemote[i] && idx == 0) || (!isRemote[i] && idx > 0)) {
+            peers[numPeers++] = i;
+        }
+    }
+    return numPeers;
+}
+
+template<unsigned int n>
+__global__ void benchBranch(const bool* in, __grid_constant__ const unsigned int idx) {
+    cuda::std::array<bool, n> isRemote{};
+    size_t start, end;
+    double duration = 0.0;
+#pragma unroll
+    for (unsigned int i = 0; i < n; ++i) {
+        isRemote[i] = in[i];
+    }
+    constexpr unsigned int runs = 4;
+#pragma unroll
+    for (unsigned int i = 0; i < runs; ++i) {
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
+        blockManipulation<len>(isRemote, idx);
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
+        duration += static_cast<double>(end - start) / static_cast<double>(runs);
+    }
+    printf("Branch less is %f, res is %u\n", duration, blockManipulation<len>(isRemote, idx));
+    duration = 0.0;
+#pragma unroll
+    for (unsigned int i = 0; i < runs; ++i) {
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
+        blockManipulationBranch<len>(isRemote, idx);
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
+        duration += static_cast<double>(end - start) / static_cast<double>(runs);
+    }
+    printf("Branch is %f, res is %u\n", duration, blockManipulationBranch<len>(isRemote, idx));
+}
+
+__always_inline
+void launchBenchBranch() {
+    boost::random::mt19937 rng(cuda::std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    const boost::random::uniform_int_distribution<> bits(0,1);
+    std::array<bool, len> b{};
+    for (unsigned int i = 0; i < len; ++i) {
+        b[i] = bits(rng);
+    }
+    //fmt::println("{}", b);
+
+    bool* bDevice;
+    constexpr unsigned int idx = 1U;
+    CHECK_ERROR_EXIT(cudaMalloc(&bDevice, sizeof(bool)*len));
+    CHECK_ERROR_EXIT(cudaMemcpy(bDevice, b.data(), sizeof(bool)*len, cudaMemcpyHostToDevice));
+    benchBranch<len><<<1,1>>>(bDevice, idx);
+    CHECK_ERROR_EXIT(cudaPeekAtLastError());
+    CHECK_ERROR_EXIT(cudaDeviceSynchronize());
+}
+
+struct __align__(16) Args {
+    double* sHeap;
+    uint64_t* flags;
+    double* result;
+    unsigned int n;
+    unsigned int rank;
+    bool remotePresent;
+    unsigned int processingRate;
+
+    Args() = default;
+    Args(double* _sHeap, uint64_t * _flags,
+        double* _result, const unsigned int& _n,
+        const unsigned int& _rank, const bool& _remotePresent, const unsigned int& _processingRate)
+        : sHeap(_sHeap),
+          flags(_flags),
+          result(_result),
+          n(_n),
+          rank(_rank),
+          remotePresent(_remotePresent),
+          processingRate(_processingRate) {}
+};
+
+__constant__ Args b{};
+void __global__ testArgs() {
+    printf("Args has rank %u, results %f\n", b.rank, b.result[0]);
+    b.sHeap[0] = 45.0;
+    b.result[0] = 59.0;
+    printf("Args has rank %u, results %f\n", b.rank, b.result[0]);
+}
+#define TO_MB(b) static_cast<double>(b) / (1024.0f*1024.0f)
+void testArgsHost() {
+    void* p;
+    CHECK_ERROR_EXIT(cudaMalloc(&p, sizeof(double)*4));
+    CHECK_ERROR_EXIT(cudaMemset(p, 0, sizeof(double)*4));
+    const auto a = Args(static_cast<double*>(p),
+        static_cast<uint64_t *>(p) + 1,
+        static_cast<double*>(p) + 2,
+        1, 0, true, 1);
+    CHECK_ERROR_EXIT(cudaMemcpyToSymbol(b, &a, sizeof(Args)));
+    testArgs<<<1,1>>>();
+    CHECK_ERROR_EXIT(cudaPeekAtLastError());
+    CHECK_ERROR_EXIT(cudaDeviceSynchronize());
+    std::cout << TO_MB(1024*1024) << std::endl;
+}
+
+auto constexpr runs = 64U;
+constexpr auto nBytes = sizeof(int);
+
+enum signal : unsigned short {
+    NOOP,
+    shouldProcess,
+    processed,
+};
+
+#define STAGES 2U
+template<typename T, unsigned int stage=0> requires (stage < STAGES)
+CUTE_DEVICE
+T* advanceHeap(const T* __restrict__ buffer, const unsigned int& bid, const unsigned int& slotSize) {
+    return buffer + slotSize * ((STAGES * bid) + stage);
+}
+
+
+template<class GEMM, unsigned short world, unsigned short rounds, bool skip=true>
+requires (GEMM::block_dim >= STAGES)
+__global__ void overlapKernel(const typename GEMM::b_value_type* __restrict__ weights,
+    const typename GEMM::c_value_type* __restrict__ result,
+    void* __restrict__ sHeap, uint64_t* __restrict__ flags,
+    CUTE_GRID_CONSTANT const int rank) {
+    // The workflow operates as follows, assuming each PE has a weight matrix and starts with an input matrix.
+    // 1. At time i A2A to disseminate vector v_i
+    // 2. GEMM on all received vectors
+    // 3. A2A to reconstitute original vector v_i
+    // 3. Process received vector
+    // 4. Repeat
+    static_assert(signal::processed == STAGES);
+    static_assert(cublasdx::size_of<GEMM>::n == cublasdx::size_of<GEMM>::k);
+
+    extern __shared__ __align__(16) char workspace[];
+    __shared__ unsigned int bid;
+    // Ensures a 32-bit single register is used
+    const unsigned int tid = cooperative_groups::thread_block::thread_rank();
+    if (tid == 0) {
+        // grid::block_rank() == peer rank
+        bid = cooperative_groups::grid_group::block_rank();
+    }
+    __threadfence_block();
+    __syncthreads();
+
+    // Make global memory tensor
+    auto tAgB = cublasdx::make_tensor(weights, GEMM::get_layout_gmem_b());
+    auto tCgC = cublasdx::make_tensor(result, GEMM::get_layout_gmem_c());
+    auto [sA, sB, sC] = GEMM::slice_shared_memory(workspace);
+
+    // Make shared memory tensor
+    auto tAsA = cublasdx::make_tensor(sA, GEMM::suggest_layout_smem_a());
+    auto tBsB = cublasdx::make_tensor(sB, GEMM::suggest_layout_smem_b());
+    auto tCsC = cublasdx::make_tensor(sC, GEMM::suggest_layout_smem_c());
+
+    // Load data from global memory tensor to shared memory tensor
+    // Note each block has identical copy of weights
+    using alignment = cublasdx::alignment_of<GEMM>;
+    cublasdx::copy<GEMM, alignment::b>(tAgB, tBsB);
+    cublasdx::copy<GEMM, alignment::b>(tCgC, tCsC);
+    cublasdx::copy_wait();
+
+    CUTE_UNROLL
+    for (unsigned short i = 0; i < rounds; ++i) {
+        // upper bound of number of messages per round
+        memcpy_async(cooperative_groups::this_thread_block(), advanceHeap(sHeap, bid, GEMM::c_size), sC, GEMM::c_size);
+        wait(cooperative_groups::this_thread_block());
+        // Communicate vector to peer
+        // TODO add send and receive cells
+        nvshmemx_putmem_signal_nbi_block(advanceHeap(sHeap, bid, GEMM::c_size),
+            advanceHeap(sHeap, bid, GEMM::c_size), (bid != rank) * GEMM::c_size,
+            flags + bid, shouldProcess, NVSHMEM_SIGNAL_SET, bid);
+
+        if (!tid) {
+            // Await data arrival
+            nvshmem_signal_wait_until(flags + bid, NVSHMEM_CMP_GT, NOOP);
+        }
+        __syncthreads();
+
+        /// First stage
+        // Copy received data to shared memory workspace
+        cooperative_groups::memcpy_async(cooperative_groups::this_thread_block(), sA,
+            advanceHeap(sHeap, bid, GEMM::c_size), GEMM::c_size);
+        wait(cooperative_groups::this_thread_block());
+        // Execute GEMM
+        GEMM().execute(GEMM::a_value_type(1.0), tAsA, tBsB, GEMM::c_value_type(0.0), tCsC);
+        __syncthreads();
+
+        memcpy_async(cooperative_groups::this_thread_block(), advanceHeap(sHeap, bid, GEMM::c_size), sC, GEMM::c_size);
+        wait(cooperative_groups::this_thread_block());
+
+        // Eagerly communicate computed vector to peer
+        nvshmemx_putmem_signal_nbi_block(advanceHeap<1>(sHeap, bid, GEMM::c_size),
+            advanceHeap<1>(sHeap, bid, GEMM::c_size), (bid != rank) * GEMM::c_size,
+            flags + world + bid, processed, NVSHMEM_SIGNAL_SET, bid);
+
+        // Second Stage
+        if (!tid) {
+            // Await data arrival
+            nvshmem_signal_wait_until(flags + world + bid, NVSHMEM_CMP_GT, NOOP);
+        }
+        __syncthreads();
+        cooperative_groups::memcpy_async(cooperative_groups::this_thread_block(), sA,
+            advanceHeap<1>(sHeap, bid, GEMM::c_size), GEMM::c_size);
+        wait(cooperative_groups::this_thread_block());
+
+        // Fused GEMM and ReLU
+        GEMM().execute(GEMM::a_value_type(1.0), tAsA, tBsB, GEMM::c_value_type(0.0), tCsC,
+            cublasdx::identity{}, cublasdx::identity{}, cublasdx::identity{},
+            cutlass::epilogue::thread::ReLU<typename GEMM::c_value_type>{});
+    }
+
+    // Store final result in global memory
+    cublasdx::copy<GEMM, alignment::c>(tCsC, tCsC);
+}
+
+void overlapPrototype() {
+    // blocks should be equal to n
+    nvshmem_init();
+    CUTE_CHECK_ERROR(cudaSetDevice(nvshmem_team_my_pe(NVSHMEMX_TEAM_NODE)));
+    auto* p = nvshmem_align(16, 2*sizeof(float)*nvshmem_n_pes());
+    CUTE_CHECK_ERROR(cudaMemset(p, 0, nBytes));
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    CUTE_CHECK_ERROR(cudaEventRecord(start));
+    CUTE_CHECK_ERROR(cudaEventRecord(stop));
+    CUTE_CHECK_ERROR(cudaPeekAtLastError());
+    CUTE_CHECK_LAST();
+    float duration = 0.0f;
+    CUTE_CHECK_ERROR(cudaEventElapsedTime(&duration, start, stop));
+    fmt::println("Elapsed time {}", duration);
+    nvshmem_free(p);
+    nvshmem_finalize();
+}
+
+void testGEMM() {
+    introduction_example<800>();
+}
+
+void testArrangement() {
+    std::array<int, 4>a{{0,1,2,3}};
+    const auto t = make_tensor(a.data(), cute::make_shape(2,2));
+    print_tensor(t);
+    fmt::println("a[0][1] is {}, a[1][1] is {}", t(0,1), t(1,1));
+    const auto tT = make_tensor(a.data(), cute::make_shape(2,2), cute::LayoutRight{});
+    print_tensor(tT);
+    fmt::println("a[0][1] is {}, a[1][1] is {}", tT(0,1), tT(1,1));
+}
+
+void testTorch() {
+    using tensorType = float;
+    constexpr unsigned int s = 4;
+    cuda::std::array<tensorType,s> aData{{0.0, 1.0, 2.0, 3.0}};
+    const auto a = torch::from_blob(aData.data(), {2,2});
+    std::cout << a << std::endl;
+    std::ranges::transform(aData.begin(), aData.end(), aData.begin(), [](const tensorType& v) {
+       return v + s;
+    });
+    const auto b = torch::from_blob(aData.data(), {2,2});
+    std::cout << b << std::endl;
+    std::cout << mm(a, b) << std::endl;
+}
+
+void tx(size_t p) {
+
+}
+int main() {
+    /*tx(q);
+    testGEMM();*/
+    auto s = signal(45);
+    std::cout << s << std::endl;
+    /*constexpr std::array<float, 2>a{{0,1}};
+    constexpr std::array<float, 2>b{{2,3}};
+    auto t = cute::make_tensor(a.data(), cute::make_shape(1,2));
+    cute::print_tensor(t);
+    t.data() = b.data();
+    cute::print_tensor(t);*/
+    //overlapPrototype();
+}
---
+++ processor/gemm.cuh
@@ -0,0 +1,98 @@
+//
+// Created by oja7 on 10/10/24.
+//
+
+#ifndef GEMM_CUH
+#define GEMM_CUH
+
+#include <cublasdx.hpp>
+#include <cuda/std/detail/libcxx/include/cstddef>
+#include <cuda/std/concepts>
+#include <cuda/std/type_traits>
+
+template<class GEMM>
+__global__ void gemm_kernel(const typename GEMM::c_value_type  alpha,
+                            const typename GEMM::a_value_type* a,
+                            const typename GEMM::b_value_type* b,
+                            const typename GEMM::c_value_type  beta,
+                            typename GEMM::c_value_type* c) {
+    extern __shared__ __align__(16) char smem[];
+
+    // Make global memory tensor
+    auto a_global_tensor = cublasdx::make_tensor(a, GEMM::get_layout_gmem_a());
+    auto b_global_tensor = cublasdx::make_tensor(b, GEMM::get_layout_gmem_b());
+    auto c_global_tensor = cublasdx::make_tensor(c, GEMM::get_layout_gmem_c());
+    if (cute::thread0()) {
+        cute::print_tensor(a_global_tensor);
+        cute::print_tensor(b_global_tensor);
+    }
+
+    // Make shared memory tensor
+    auto [smem_a, smem_b, smem_c] = GEMM::slice_shared_memory(smem);
+    auto a_shared_tensor = cublasdx::make_tensor(smem_a, GEMM::suggest_layout_smem_a());
+    auto b_shared_tensor = cublasdx::make_tensor(smem_b, GEMM::suggest_layout_smem_b());
+    auto c_shared_tensor = cublasdx::make_tensor(smem_c, GEMM::suggest_layout_smem_c());
+
+    // Load data from global memory tensor to shared memory tensor
+    using alignment = cublasdx::alignment_of<GEMM>;
+    cublasdx::copy<GEMM, alignment::a>(a_global_tensor, a_shared_tensor);
+    cublasdx::copy<GEMM, alignment::b>(b_global_tensor, b_shared_tensor);
+    cublasdx::copy<GEMM, alignment::c>(c_global_tensor, c_shared_tensor);
+    cublasdx::copy_wait();
+
+    // Execute GEMM
+    GEMM().execute(alpha, a_shared_tensor, b_shared_tensor, beta, c_shared_tensor);
+    __syncthreads();
+    if (cute::thread0()) {
+        cute::print_tensor(c_shared_tensor);
+    }
+
+    // Store data from shared memory tensor to global memory tensor
+    cublasdx::copy<GEMM, alignment::c>(c_shared_tensor, c_global_tensor);
+}
+
+template<unsigned int Arch=800>
+int introduction_example() {
+    constexpr auto M = 2U;
+    constexpr auto N = 2U;
+    constexpr auto K = 2U;
+    using value_type = float;
+    using GEMM = decltype(cublasdx::Size<M, N, K>()
+                  + cublasdx::Precision<value_type>()
+                  + cublasdx::Type<cublasdx::type::real>()
+                  + cublasdx::Arrangement<cublasdx::row_major, cublasdx::col_major, cublasdx::row_major>()
+                  + cublasdx::Function<cublasdx::function::MM>()
+                  + cublasdx::SM<Arch>()
+                  + cublasdx::Block());
+    static_assert(cublasdx::is_complete_blas_execution<GEMM>::value);
+
+    constexpr auto global_a_size = M*K;
+    constexpr auto global_b_size = K*N;
+    constexpr auto global_c_size = M*N;
+
+    // Allocate managed memory for A, B, C matrices in one go
+    value_type* abc;
+    constexpr auto        size       = global_a_size + global_b_size + global_c_size;
+    constexpr auto        size_bytes = size * sizeof(value_type);
+    CUTE_CHECK_ERROR(cudaMallocManaged(&abc, size_bytes));
+    // Generate data
+    for (uint i = 0; i < global_a_size + global_b_size; i++) {
+        abc[i] = static_cast<value_type>(i);
+    }
+
+    const value_type* a = abc;
+    const value_type* b = abc + global_a_size;
+    value_type* c = abc + global_a_size + global_b_size;
+
+    // Invokes kernel with GEMM::block_dim threads in CUDA block
+    gemm_kernel<GEMM><<<1, GEMM::block_dim, GEMM::shared_memory_size>>>(1.0, a, b, 0.0, c);
+    CUTE_CHECK_ERROR(cudaPeekAtLastError());
+    CUTE_CHECK_ERROR(cudaDeviceSynchronize());
+    std::array<value_type, M*N> result {};
+    CUTE_CHECK_ERROR(cudaMemcpy(result.data(), c, sizeof(value_type)*global_c_size, cudaMemcpyDeviceToHost));
+    CUTE_CHECK_ERROR(cudaFree(abc));
+    std::cout << "Success" << std::endl;
+    fmt::println("GEMM Results {}", result);
+    return 0;
+}
+#endif //GEMM_CUH

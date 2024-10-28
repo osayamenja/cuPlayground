d5d4f099e41cbf67203f7a57b0b18b70fd1f47eb
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2024-10-27 22:29:17 -0400

    Complete GEMM Template Parameters

--- processor/tiling.cuh
+++ processor/tiling.cuh
@@ -11,11 +11,13 @@
 #include <cute/arch/copy_sm80.hpp>
 
 // GEMM configuration constants
-#define MIN_ARCH 700
-#define THREADS 128
-#define BLOCK_M 128
-#define BLOCK_N 128
-#define BLOCK_K 8
+#define MIN_ARCH 700U
+#define THREADS 128U
+#define BLOCK_M 128U
+#define BLOCK_N 64U
+#define BLOCK_K_HALF 32U
+#define BLOCK_K_FULL 16U
+#define MAX_REGS 64U
 
 template<unsigned int Arch, typename TC, typename TA=TC, typename TB=TA>
 struct MMAConfig {
@@ -26,73 +28,95 @@ struct MMAConfig {
 
 template<>
 struct MMAConfig<700, cute::half_t> {
-    using mma = void;
+    using mma = cute::TiledMMA<
+      cute::MMA_Atom<cute::SM70_8x8x4_F16F16F16F16_TN>,
+      cute::Layout<cute::Shape<cute::_4, cute::_4, cute::_1>>>;
 };
 
 template<>
 struct MMAConfig<700, float, cute::half_t> {
-    using mma = void;
+    using mma = cute::TiledMMA<
+      cute::MMA_Atom<cute::SM70_8x8x4_F32F16F16F32_TN>,
+      cute::Layout<cute::Shape<cute::_4, cute::_4, cute::_1>>>;
 };
 
 template<>
 struct MMAConfig<800, cute::half_t> {
-    using mma = void;
+    using mma = cute::TiledMMA<
+      cute::MMA_Atom<cute::SM80_16x8x16_F16F16F16F16_TN>,
+      cute::Layout<cute::Shape<cute::_4, cute::_1, cute::_1>>>;
 };
 
 template<>
 struct MMAConfig<800, float, cute::half_t> {
-    using mma = void;
+    using mma = cute::TiledMMA<
+      cute::MMA_Atom<cute::SM80_16x8x16_F32F16F16F32_TN>,
+      cute::Layout<cute::Shape<cute::_4, cute::_1, cute::_1>>>;
 };
 
 template<>
 struct MMAConfig<800, float, cute::bfloat16_t> {
-    using mma = void;
+    using mma = cute::TiledMMA<
+      cute::MMA_Atom<cute::SM80_16x8x16_F32BF16BF16F32_TN>,
+      cute::Layout<cute::Shape<cute::_4, cute::_1, cute::_1>>>;
 };
 
 template<>
 struct MMAConfig<800, float, cute::tfloat32_t> {
-    using mma = void;
+    // TODO this may be incorrect
+    using mma = cute::TiledMMA<
+      cute::MMA_Atom<cute::SM80_16x8x8_F32TF32TF32F32_TN>,
+      cute::Layout<cute::Shape<cute::_4, cute::_1, cute::_1>>>;
+};
+
+template <cublasdx::arrangement a, unsigned int sizeK>
+struct SwizzleAtom;
+
+template<>
+struct SwizzleAtom<cublasdx::arrangement::row_major, BLOCK_K_HALF> {
+    using swizzleAtom =  decltype(
+    composition(cute::Swizzle<2,3,3>{},
+                cute::Layout<cute::Shape < cute::_8, cute::_32>,
+                       cute::Stride<cute::_32, cute::_1>>{}));
 };
 
 template<>
-struct MMAConfig<900, cute::half_t> {
-    using mma = void;
+struct SwizzleAtom<cublasdx::arrangement::col_major, BLOCK_K_HALF> {
+    // Weird combination but such is life
+    using swizzleAtom =  decltype(
+    composition(cute::Swizzle<3,3,3>{},
+                cute::Layout<cute::Shape <cute::_64, cute::_8>,
+                       cute::Stride< cute::_1,cute::_64>>{}));
 };
 
 template<>
-struct MMAConfig<900, float, cute::half_t> {
-    using mma = void;
+struct SwizzleAtom<cublasdx::arrangement::row_major, BLOCK_K_FULL> {
+    using swizzleAtom =  decltype(
+    composition(cute::Swizzle<3,2,3>{},
+                cute::Layout<cute::Shape < cute::_8,cute::_32>,
+                       cute::Stride<cute::_32, cute::_1>>{}));
 };
 
 template<>
-struct MMAConfig<900, float, cute::bfloat16_t> {
-    using mma = void;
+struct SwizzleAtom<cublasdx::arrangement::col_major, BLOCK_K_FULL> {
+    // Weird combination but such is life
+    using swizzleAtom =  decltype(
+    composition(cute::Swizzle<3,2,3>{},
+                cute::Layout<cute::Shape <cute::_32, cute::_8>,
+                       cute::Stride< cute::_1,cute::_32>>{}));
 };
 
-template<>
-struct MMAConfig<900, float, cute::tfloat32_t> {
-    using mma = void;
+
+enum class LayoutOptimization {
+  UseSwizzle,
+    UseVanilla
 };
 
-template<>
-struct MMAConfig<900, float, cute::float_e4m3_t> {
-    using mma = void;
-};
-
-template<>
-struct MMAConfig<900, float, cute::float_e4m3_t, cute::float_e5m2_t> {
-    using mma = void;
-};
-
-template<>
-struct MMAConfig<900, float, cute::float_e5m2_t> {
-    using mma = void;
-};
-
-template<class GEMM>
+template<class GEMM, LayoutOptimization lOpt = LayoutOptimization::UseVanilla>
 requires (cublasdx::is_complete_blas<GEMM>::value
 && cublasdx::is_supported<GEMM, cublasdx::sm_of<GEMM>::value>::value
-&& cublasdx::sm_of<GEMM>::value >= MIN_ARCH  && cublasdx::sm_of<GEMM>::value < 900)
+&& cublasdx::sm_of<GEMM>::value >= MIN_ARCH
+&& cublasdx::sm_of<GEMM>::value < 900)
 struct CollectiveMMAConfig{
     using strideA = cuda::std::conditional_t<cublasdx::arrangement_of<GEMM>::a == cublasdx::row_major,
     cute::GenRowMajor, cute::GenColMajor>;
@@ -105,9 +129,11 @@ struct CollectiveMMAConfig{
     using tLayX = cute::_16;
     using tLayY = cute::_8;
 
-    static_assert(cublasdx::size_of<GEMM>::m % tLayX::value == 0);
-    static_assert(cublasdx::size_of<GEMM>::n % tLayX::value == 0);
+    static_assert(cublasdx::size_of<GEMM>::m % tLayX::value == 0 && cublasdx::size_of<GEMM>::m == BLOCK_M);
+    static_assert(cublasdx::size_of<GEMM>::n % tLayX::value == 0 && cublasdx::size_of<GEMM>::n == BLOCK_N);
     static_assert(cublasdx::size_of<GEMM>::k % tLayY::value == 0);
+    static_assert(sizeof(GEMM::a_value_type) <= 2 && sizeof(GEMM::b_value_type) <= 2 ?
+        cublasdx::size_of<GEMM>::k == BLOCK_K_HALF : cublasdx::size_of<GEMM>::k == BLOCK_K_FULL);
 
     using vLayX = cute::Int<sizeof(cute::uint128_t) / sizeof(GEMM::a_value_type)>;
     using vLayY = cute::Int<cublasdx::size_of<GEMM>::k / tLayY::value>;
@@ -145,25 +171,24 @@ struct CollectiveMMAConfig{
     using sCopyB = cute::Copy_Atom<cute::SM75_U32x4_LDSM_N, typename GEMM::b_value_type>;
     using sCopyC = cute::Copy_Atom<cute::AutoVectorizingCopyWithAssumedAlignment<>, typename GEMM::c_value_type>;
 
-    // TODO upgrade below layouts to Swizzle
-    using sLayA = cute::Layout<cute::Shape<cute::Int<cublasdx::size_of<GEMM>::m>, cute::Int<cublasdx::size_of<GEMM>::k>>,
+    using vSLayA = cute::Layout<cute::Shape<cute::Int<cublasdx::size_of<GEMM>::m>, cute::Int<cublasdx::size_of<GEMM>::k>>,
     cuda::std::conditional_t<cublasdx::arrangement_of<GEMM>::a == cublasdx::arrangement::col_major,
     cute::GenColMajor, cute::GenRowMajor>>;
-    using sLayB = cute::Layout<cute::Shape<cute::Int<cublasdx::size_of<GEMM>::n>, cute::Int<cublasdx::size_of<GEMM>::k>>,
+    using sLayA = cuda::std::conditional_t<lOpt == LayoutOptimization::UseSwizzle,
+    SwizzleAtom<cublasdx::arrangement_of<GEMM>::a, cublasdx::size_of<GEMM>::k>, vSLayA>;
+
+    using vSLayB = cute::Layout<cute::Shape<cute::Int<cublasdx::size_of<GEMM>::n>, cute::Int<cublasdx::size_of<GEMM>::k>>,
     cuda::std::conditional_t<cublasdx::arrangement_of<GEMM>::b == cublasdx::arrangement::col_major,
     cute::GenColMajor, cute::GenRowMajor>>;
+    using sLayB = cuda::std::conditional_t<lOpt == LayoutOptimization::UseSwizzle,
+    SwizzleAtom<cublasdx::arrangement_of<GEMM>::a, cublasdx::size_of<GEMM>::k>, vSLayA>;
+
     using sLayC = cute::Layout<cute::Shape<cute::Int<cublasdx::size_of<GEMM>::m>, cute::Int<cublasdx::size_of<GEMM>::n>>,
     cuda::std::conditional_t<cublasdx::arrangement_of<GEMM>::c == cublasdx::arrangement::col_major,
     cute::GenColMajor, cute::GenRowMajor>>;
 
-    using config = cublasdx::detail::layout_database::optimal_config<THREADS, cublasdx::sm_of<GEMM>::value,
-    typename GEMM::a_value_type, cublasdx::arrangement_of<GEMM>::a == cublasdx::arrangement::col_major, cublasdx::alignment_of<GEMM>::a,
-    typename GEMM::b_value_type, cublasdx::arrangement_of<GEMM>::b == cublasdx::arrangement::col_major, cublasdx::alignment_of<GEMM>::b,
-    typename GEMM::c_value_type, cublasdx::arrangement_of<GEMM>::c == cublasdx::arrangement::col_major, cublasdx::alignment_of<GEMM>::c,
-    cublasdx::size_of<GEMM>::m, cublasdx::size_of<GEMM>::n, cublasdx::size_of<GEMM>::k>;
-
-    using mma_t = MMAConfig<cublasdx::sm_of<GEMM>::value, typename GEMM::a_value_type, typename GEMM::b_value_type,
-    typename GEMM::c_value_type>;
+    using mma_t = MMAConfig<cublasdx::sm_of<GEMM>::value, typename GEMM::c_value_type, typename GEMM::a_value_type,
+    typename GEMM::b_value_type>;
 };
 
 #endif //TILING_CUH

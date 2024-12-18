0296156296c919ffe813ae9c36adacf336ba812b
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2024-12-18 14:50:28 -0500

    Linting

--- main.cu
+++ main.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <cuda/std/cassert>
 #include <cuda/std/array>
 #include <fmt/ranges.h>
 #include <nvshmemx.h>
@@ -9,31 +10,34 @@
 #include <cuda/experimental/device.cuh>
 #include "util.cuh"
 
-template<unsigned int x = 2>
 struct Task {
     // TODO use cuda std array
-    cuda::std::array<uint*, x> f{};
+    cuda::std::array<uint*, 2> f = {};
 
     __device__ __forceinline__
     Task() = default;
+
     __device__ __forceinline__
-    explicit Task(const cuda::std::array<uint*, x>& _f) {
-        #pragma unroll
-        for (uint i = 0; i < x; ++i) {
-            f[i] = _f[i];
-        }
-    }
+    explicit Task(const cuda::std::array<uint*, 2>& _f) : f(_f) {}
 };
 
-__global__ void atf(uint* a) {
+__global__ void theatre(uint* a) {
+    __shared__ Task t;
+    a[0] = 46;
+    a[1] = 89;
     a[2] = 45;
-    const cuda::std::array<uint*, 2> arr {a, a + 4};
-    auto t = Task(arr);
-    assert(t[0][2] == a[2]);
+    a[3] = 90;
+
+    cuda::std::array<uint*, 2> arr {{a, a + 2}};
+    t = Task(arr);
+    static_assert(sizeof(Task) == sizeof(decltype(arr)::value_type) * arr.size());
+    assert(t.f[0][1] == a[1]);
+    t.f = arr;
+    assert(t.f[0][3] == 90);
 }
 int main() {
     uint* p;
     CHECK_ERROR_EXIT(cudaMalloc(&p, 8 * sizeof(unsigned int)));
-    atf<<<1,1>>>(p);
+    theatre<<<1,1>>>(p);
     CHECK_LAST();
 }

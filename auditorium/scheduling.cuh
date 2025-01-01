d009d6831081e869a412c773370e8bcb740ec879
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2025-01-01 15:06:33 -0500

    Bench atomic load

--- auditorium/scheduling.cuh
+++ auditorium/scheduling.cuh
@@ -5,24 +5,20 @@
 #ifndef SCHEDULING_CUH
 #define SCHEDULING_CUH
 
-#include <cuda/std/array>
 #include <cute/numeric/math.hpp>
 #include <cutlass/array.h>
 #include "../util.cuh"
 
-template<unsigned int processorCount, typename Registers, typename RScratch>
-requires(processorCount > 0 && isRegisterV<Registers> && isRegisterV<RScratch>)
+template<unsigned int processorCount, unsigned int rQSetSize>
+requires(processorCount > 0  && rQSetSize > 1 && cutlass::ispow2(rQSetSize))
 __device__ __forceinline__
 void scheduleLoop(unsigned int& tasks,
     unsigned int& scheduled,
-    const unsigned int& wSIdx,
-    Registers& rtQTails,
-    RScratch& rQSet,
+    unsigned int& rtQTail,
     unsigned int* __restrict__ const& rQHead,
     unsigned int& rQTail,
     const unsigned int* __restrict__ const& rQ,
-    const unsigned int& tQRow,
-    const unsigned int& tQRl,
+    const unsigned int& tQIdx,
     unsigned int* __restrict__ const& pDB) {
     while (tasks) {
         const auto readyProcesses = atomicLoad<cuda::thread_scope_block>(rQHead) - rQTail;
@@ -33,45 +29,33 @@ void scheduleLoop(unsigned int& tasks,
             // ensures reads to ready Q happen after signal reception
             __threadfence_block();
         }
-        constexpr auto batchSize = RScratch::kElements;
-        const auto tSb = tasksToSchedule / batchSize; // tasks to schedule batches
+        const auto tSb = tasksToSchedule / rQSetSize; // tasks to schedule batches
         for (uint i = 0; i < tSb; ++i) {
             #pragma unroll
-            for (uint j = 0; j < batchSize; ++j) {
-                rQSet[j] = rQ[rQTail++ % processorCount];
-            }
-
-            #pragma unroll
-            for (uint j = 0; j < batchSize; ++j) {
-                const auto pid = rQSet[j];
-                ++rtQTails[wSIdx];
-                atomicExch(pDB + pid, tQRow * tQRl + rtQTails[wSIdx]);
+            for (uint j = 0; j < rQSetSize; ++j) {
+                const auto pid = rQ[rQTail++ % processorCount];
+                ++rtQTail;
+                atomicExch(pDB + pid, tQIdx + rtQTail);
             }
         }
-        const auto residue = tasksToSchedule - tSb * batchSize;
+        const auto residue = tasksToSchedule - tSb * rQSetSize;
         for (uint i = 0; i < residue; ++i) {
-            rQSet[i] = rQ[rQTail++ % processorCount];
-        }
-
-        for (uint i = 0; i < residue; ++i) {
-            const auto pid = rQSet[i];
-            ++rtQTails[wSIdx];
+            const auto pid = rQ[rQTail++ % processorCount];
+            ++rtQTail;
             // Fence is not needed prior to signal given task producers already issue one.
-            atomicExch(pDB + pid, tQRow * tQRl + rtQTails[wSIdx]);
+            atomicExch(pDB + pid, tQIdx + rtQTail);
         }
     }
 }
 
 // all loops are unrolled
-template<unsigned int processorCount, unsigned int producerCount, unsigned int wSetSize,
-typename Registers, typename RScratch>
+template<unsigned int processorCount, unsigned int producerCount, unsigned int wSetSize, unsigned int rQSetSize,
+typename Registers>
 requires(processorCount > 0 && producerCount > 0 && producerCount < 128 && wSetSize > 1 && wSetSize % 4 == 0
-    && isRegisterV<Registers> && Registers::kElements == wSetSize
-    && isRegisterV<RScratch>)
+    && isRegisterV<Registers> && Registers::kElements == wSetSize)
 __device__ __forceinline__
 void staticSchedule(unsigned int& scheduled,
     Registers& rtQTails,
-    RScratch& rQSet,
     const unsigned int& tQRl,
     unsigned int* __restrict__ const& tQTails,
     unsigned int* __restrict__ const& tQHeads,
@@ -93,8 +77,8 @@ void staticSchedule(unsigned int& scheduled,
             const auto tQRow = j + i * wSetSize;
             auto tasks = atomicLoad<cuda::thread_scope_block>(tQHeads + tQRow) - rtQTails[j];
             // Let's schedule these tasks
-            scheduleLoop<processorCount>(tasks, scheduled, j, rtQTails, rQSet, rQHead,
-                rQTail, rQ, tQRow, tQRl, pDB);
+            scheduleLoop<processorCount, rQSetSize>(tasks, scheduled, rtQTails[j], rQHead,
+                rQTail, rQ, tQRow * tQRl, pDB);
         }
 
         // persist wQ tails
@@ -115,8 +99,8 @@ void staticSchedule(unsigned int& scheduled,
     for (uint j = 0; j < residue; ++j) {
         const auto tQRow = j + producerBatches * wSetSize;
         auto tasks = atomicLoad<cuda::thread_scope_block>(tQHeads + tQRow) - rtQTails[j];
-        scheduleLoop<processorCount>(tasks, scheduled, j, rtQTails, rQSet, rQHead,
-                rQTail, rQ, tQRow, tQRl, pDB);
+        scheduleLoop<processorCount, rQSetSize>(tasks, scheduled, rtQTails[j], rQHead,
+                rQTail, rQ, tQRow * tQRl, pDB);
     }
 
     #pragma unroll
@@ -127,14 +111,12 @@ void staticSchedule(unsigned int& scheduled,
 }
 
 // dynamic trip, used for external task queue whose heads are in global memory not shared.
-template<unsigned int processorCount, unsigned int wSetSize, typename Registers, typename RScratch>
+template<unsigned int processorCount, unsigned int wSetSize, unsigned int rQSetSize, typename Registers>
 requires(processorCount > 0 && wSetSize > 1 && wSetSize % 4 == 0
-    && isRegisterV<Registers> && Registers::kElements == wSetSize
-    && isRegisterV<RScratch>)
+    && isRegisterV<Registers> && Registers::kElements == wSetSize)
 __device__ __forceinline__
 void dynamicSchedule(unsigned int& scheduled,
     Registers& rtQTails,
-    RScratch& rQSet,
     const unsigned int& tQRl,
     unsigned int* __restrict__ const& tQTails,
     unsigned int* __restrict__ const& tQHeads,
@@ -156,8 +138,8 @@ void dynamicSchedule(unsigned int& scheduled,
             const auto tQRow = j + i * wSetSize;
             auto tasks = atomicLoad(tQHeads + tQRow) - rtQTails[j];
             // Let's schedule these tasks
-            scheduleLoop<processorCount>(tasks, scheduled, j, rtQTails, rQSet, rQHead,
-                rQTail, rQ, tQRow, tQRl, pDB);
+            scheduleLoop<processorCount, rQSetSize>(tasks, scheduled, rtQTails[j], rQHead,
+                rQTail, rQ, tQRow * tQRl, pDB);
         }
 
         // persist wQ tails
@@ -175,8 +157,8 @@ void dynamicSchedule(unsigned int& scheduled,
     for (uint j = 0; j < residue; ++j) {
         const auto tQRow = j + mailboxBatches * wSetSize;
         auto tasks = atomicLoad(tQHeads + tQRow) - rtQTails[j];
-        scheduleLoop<processorCount>(tasks, scheduled, j, rtQTails, rQSet, rQHead,
-                rQTail, rQ, tQRow, tQRl, pDB);
+        scheduleLoop<processorCount, rQSetSize>(tasks, scheduled, rtQTails[j], rQHead,
+                rQTail, rQ, tQRow * tQRl, pDB);
     }
 
     for (uint j = 0; j < residue; ++j) {
@@ -203,18 +185,105 @@ void schedulerStart(const unsigned int& tQRl,
     unsigned int scheduled = 0U;
     unsigned int rQTail = 0U;
     constexpr auto wSetSize = 16;
-    constexpr auto rQSetSize = 16;
     cutlass::AlignedArray<unsigned int, wSetSize> rtQTails{};
-    cutlass::AlignedArray<unsigned int, rQSetSize> rQSet{};
     rtQTails.fill(0U);
 
     while (scheduled < atomicLoad<cuda::thread_scope_block>(taskBound)) {
+        constexpr auto rQSetSize = 16;
         // sweep all static wQHeads and schedule pending tasks
-        staticSchedule<processorCount, producerCount, wSetSize>(scheduled, rtQTails, rQSet, tQRl, tQTails,
+        staticSchedule<processorCount, producerCount, wSetSize, rQSetSize>(scheduled, rtQTails,tQRl, tQTails,
             tQHeads, rQHead, rQTail, rQ, pDB);
         // Now do dynamic scheduling
-        dynamicSchedule<processorCount, wSetSize>(scheduled, rtQTails, rQSet, gtQRl, gtQTails,
+        dynamicSchedule<processorCount, wSetSize, rQSetSize>(scheduled, rtQTails, gtQRl, gtQTails,
             gtQHeads, rQHead, rQTail, rQ, pDB, gtQCL);
     }
+}
+
+template<
+    unsigned int processorCount,
+    unsigned int producerCount,
+    unsigned int M,
+    unsigned int N,
+    unsigned int Nx,
+    unsigned int bM,
+    unsigned int bN
+>
+__global__ __maxnreg__(128) void sK(unsigned int* __restrict__ p, const bool skip = true) {
+    constexpr auto fTB = M / bM * (Nx / bN);
+    constexpr auto sTB = M / bM * (N / bN);
+    constexpr auto tQRl = cute::ceil_div(fTB, producerCount);
+    constexpr auto gtQCl = M / bM;
+    constexpr auto gtQRl = N / bN;
+
+    constexpr auto sharedSize = (2 + 2 * producerCount + gtQCl + processorCount) * sizeof(unsigned int);
+    __shared__ __align__(16) cuda::std::byte scratch [sharedSize];
+
+    auto* __restrict__ taskBound = CAST_TO(unsigned int, scratch);
+    *taskBound = fTB + sTB;
+    auto* __restrict__ tQHeads = taskBound + 1;
+    auto* __restrict__ tQTails = tQHeads + producerCount;
+    #pragma unroll
+    for (uint i = threadIdx.x; i < producerCount; i += blockDim.x) {
+        tQHeads[i] = fTB / producerCount;
+        constexpr auto residue = fTB - fTB / producerCount * producerCount;
+        tQHeads[i] += static_cast<unsigned int>(i < residue);
+        // clear state
+        tQTails[i] = 0U;
+    }
+    #pragma unroll
+    for (uint i = threadIdx.x; i < gtQCl; i += blockDim.x) {
+        p[i] = gtQRl;
+    }
+
+    auto* __restrict__ gTQTails = tQTails + producerCount;
+    auto* __restrict__ rQHead = gTQTails + gtQCl;
+    *rQHead = fTB + sTB; // this is actually cheating
+    const auto* __restrict__ rQ = rQHead + 1;
+    #pragma unroll
+    for (uint i = threadIdx.x; i < processorCount; i += blockDim.x) {
+        // done this way to preserve the const attribute of rQ
+        (rQHead + 1)[i] = i;
+    }
+
+    #pragma unroll
+    for (uint i = threadIdx.x; i < gtQCl; i += blockDim.x) {
+        // clear state
+        gTQTails[i] = 0U;
+    }
+
+    __syncthreads();
+    if (!threadIdx.x) {
+        uint64_t start, end;
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
+        schedulerStart<processorCount, producerCount>(tQRl, gtQCl, gtQRl,
+                tQHeads, p, gTQTails, tQTails, taskBound, rQHead, rQ, p + gtQCl);
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
+        if(!skip) {
+            printf("Time taken is %fus\n", static_cast<float>(end - start) / 1000.0f);
+        }
+    }
+}
+
+__host__ __forceinline__
+void hostSchedule() {
+    constexpr auto M = 8192U;
+    constexpr auto N = 4096U;
+    constexpr auto Nx = 4096U;
+    constexpr auto bM = 128U;
+    constexpr auto bN = 64U;
+
+    constexpr auto processorCount = 4 * 108U; // Ampere with <= 128 registers
+    constexpr auto threads = 128;
+    constexpr auto producerCount = threads - 2;
+    constexpr auto gtQCl = M / bM;
+
+    unsigned int* p;
+    CHECK_ERROR_EXIT(cudaMallocAsync(&p, (processorCount + gtQCl) * sizeof(unsigned int), cudaStreamPerThread));
+    for (uint i = 0; i < 256; ++i) {
+        sK<processorCount, producerCount, M, N, Nx, bM, bN><<<1, 256, 0, cudaStreamPerThread>>>(p);
+    }
+    sK<processorCount, producerCount, M, N, Nx, bM, bN><<<1, 256, 0, cudaStreamPerThread>>>(p, false);
+    CHECK_ERROR_EXIT(cudaFreeAsync(p, cudaStreamPerThread));
+    CHECK_LAST();
 }
 #endif //SCHEDULING_CUH
--- main.cu
+++ main.cu
@@ -1,88 +1,67 @@
-#include <cuda/std/cstddef>
-#include <cute/int_tuple.hpp>
+#include <cub/cub.cuh>
 #include "util.cuh"
-#include "auditorium/scheduling.cuh"
 
-__global__ __maxnreg__(128) void theatre(unsigned int* __restrict__ p, const bool skip = true) {
-    constexpr auto processorCount = 4 * 108U;
-    constexpr auto producerCount = 126;
-
-    constexpr auto M = 8192U;
-    constexpr auto N = 4096U;
-    constexpr auto Nx = 4096U;
-    constexpr auto bM = 128U;
-    constexpr auto bN = 64U;
-
-    constexpr auto fTB = M / bM * (Nx / bN);
-    constexpr auto sTB = M / bM * (N / bN);
-    constexpr auto tQRl = cute::ceil_div(fTB, producerCount);
-    constexpr auto gtQCl = M / bM;
-    constexpr auto gtQRl = N / bN;
-
-    constexpr auto sharedSize = (2 + 2 * producerCount + gtQCl + processorCount) * sizeof(unsigned int);
-    __shared__ __align__(16) cuda::std::byte scratch [sharedSize];
-
-    auto* __restrict__ taskBound = CAST_TO(unsigned int, scratch);
-    *taskBound = fTB + sTB;
-    auto* __restrict__ tQHeads = taskBound + 1;
-    auto* __restrict__ tQTails = tQHeads + producerCount;
-    #pragma unroll
-    for (uint i = threadIdx.x; i < producerCount; i += blockDim.x) {
-        tQHeads[i] = fTB / producerCount;
-        constexpr auto residue = fTB - fTB / producerCount * producerCount;
-        tQHeads[i] += static_cast<unsigned int>(i < residue);
-        // clear state
-        tQTails[i] = 0U;
-    }
-    #pragma unroll
-    for (uint i = threadIdx.x; i < gtQCl; i += blockDim.x) {
-        p[i] = gtQRl;
-    }
-
-    auto* __restrict__ gTQTails = tQTails + producerCount;
-    auto* __restrict__ rQHead = gTQTails + gtQCl;
-    *rQHead = fTB + sTB; // this is actually cheating
-    const auto* __restrict__ rQ = rQHead + 1;
-    #pragma unroll
-    for (uint i = threadIdx.x; i < processorCount; i += blockDim.x) {
-        // done this way to preserve the const attribute of rQ
-        (rQHead + 1)[i] = i;
-    }
-
-    #pragma unroll
-    for (uint i = threadIdx.x; i < gtQCl; i += blockDim.x) {
-        // clear state
-        gTQTails[i] = 0U;
-    }
-
-    __syncthreads();
+template<unsigned int threads>
+__global__ void theatre(unsigned int* __restrict__ p, const bool skip = true) {
+    __shared__ unsigned int pS;
+    cuda::atomic_ref aSp{pS};
+    cuda::atomic_ref aGp{*p};
     if (!threadIdx.x) {
-        uint64_t start, end;
+        pS = threadIdx.x;
+    }
+    __syncthreads();
+
+    double myClocked[2] = {0.0, 0.0};
+    double theirsClocked[2] = {0.0, 0.0};
+    constexpr auto rounds = 64;
+    for (uint i = 0; i < rounds; ++i) {
+        uint64_t start = 0, end = 0;
         asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
-        schedulerStart<processorCount, producerCount>(tQRl, gtQCl, gtQRl,
-                tQHeads, p, gTQTails, tQTails, taskBound, rQHead, rQ, p + gtQCl);
+        cuda::std::ignore = atomicLoad<cuda::thread_scope_block>(&pS);
         asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
-        if(!skip) {
-            printf("Time taken is %fus\n", static_cast<float>(end - start) / 1000.0f);
-        }
+        myClocked[0] += static_cast<double>(end - start) / static_cast<double>(rounds);
+
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
+        cuda::std::ignore = aSp.load();
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
+        theirsClocked[0] += static_cast<double>(end - start) / static_cast<double>(rounds);
+
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
+        cuda::std::ignore = atomicLoad(p);
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
+        myClocked[1] += static_cast<double>(end - start) / static_cast<double>(rounds);
+
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(start)::);
+        cuda::std::ignore = aGp.load();
+        asm volatile("mov.u64 %0, %%globaltimer;": "=l"(end)::);
+        theirsClocked[1] += static_cast<double>(end - start) / static_cast<double>(rounds);
+    }
+    using BlockReduce = cub::BlockReduce<double, threads>;
+    __shared__ typename BlockReduce::TempStorage temp_storage;
+
+    myClocked[0] = BlockReduce(temp_storage).Reduce(myClocked[0], cub::Max());
+    myClocked[1] = BlockReduce(temp_storage).Reduce(myClocked[1], cub::Max());
+    theirsClocked[0] = BlockReduce(temp_storage).Reduce(theirsClocked[0], cub::Max());
+    theirsClocked[1] = BlockReduce(temp_storage).Reduce(theirsClocked[1], cub::Max());
+
+    if (!threadIdx.x && !skip) {
+        printf("Block %u, sAtomicLoad: %fns, gAtomicLoad: %fns, sCudaLoad: %fns, gCudaLoad: %fns\n",
+            blockIdx.x, myClocked[0], myClocked[1], theirsClocked[0], theirsClocked[1]);
     }
 }
 
+__host__ __forceinline__
+void hostTheatre() {
+    unsigned int* p;
+    CHECK_ERROR_EXIT(cudaMalloc(&p, sizeof(unsigned int)));
+    constexpr auto threads = 128;
+    /*for (uint i = 0; i < 128; ++i) {
+        theatre<<<128, 128>>>(p);
+    }*/
+    theatre<threads><<<128, threads>>>(p);
+    CHECK_LAST();
+}
 
 int main() {
-    constexpr auto M = 8192U;
-    constexpr auto bM = 128U;
-
-    constexpr auto processorCount = 4 * 108U;
-    constexpr auto gtQCl = M / bM;
-
-    unsigned int* p;
-    CHECK_ERROR_EXIT(cudaMallocAsync(&p, (processorCount + gtQCl) * sizeof(unsigned int), cudaStreamPerThread));
-    for (uint i = 0; i < 256; ++i) {
-        theatre<<<1, 256, 0, cudaStreamPerThread>>>(p);
-    }
-    theatre<<<1, 256, 0, cudaStreamPerThread>>>(p, false);
-    CHECK_ERROR_EXIT(cudaFreeAsync(p, cudaStreamPerThread));
-    CHECK_LAST();
-    //hostCombine();
+    hostTheatre();
 }

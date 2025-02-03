bc25f1f727d5d62ab58fd8153a82b05abdacc386
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2025-02-02 22:57:59 -0500

    Try out LibTorch API

--- main.cu
+++ main.cu
@@ -1,6 +1,35 @@
-#include <torch/torch.h>
+#include <array>
 #include <iostream>
+
+#include <torch/torch.h>
+
+struct Expert final : torch::nn::Module {
+    torch::nn::Linear g1;
+    torch::nn::Linear g2;
+
+    Expert():
+          g1(2, 4),
+          g2(4, 2) {
+        register_module("g1", g1);
+        register_module("g2", g2);
+    }
+
+    torch::Tensor forward(torch::Tensor& x) {
+        x = relu(g1->forward(x));
+        return g2->forward(x);
+    }
+};
+
 int main() {
-    const torch::Tensor tensor = torch::rand({2, 3});
+    std::array<float, 4> a{0, 1, 2, 3};
+    torch::Device device(torch::kCUDA);
+    const torch::Tensor tensor = torch::from_blob(a.data(), {2,2}).to(device);
+    const Expert model;
+    std::cout << model << std::endl;
+    for (const auto& p : model.named_parameters()) {
+        std::cout << p.key() << std::endl;
+        std::cout << p.value() << std::endl;
+        std::cout << p.value().data()[0] << std::endl;
+    }
     std::cout << tensor << std::endl;
 }
